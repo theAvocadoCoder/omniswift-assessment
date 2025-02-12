49daccd39e0bdd9a90bbcd7abd1683f2939c959c
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2025-02-12 11:38:50 +0100

    Configure tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <h1 data-testid="helloWorld">Hello, world!</h1>
+      <h1 className="text-blue-600" data-testid="helloWorld">Hello, world!</h1>
     </>
   )
 }
