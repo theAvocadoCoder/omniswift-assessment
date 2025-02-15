68e28e98f252e565879947e4d30ac80a3a44256d
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2025-02-15 21:34:34 +0100

    Setup an API service for querying and filtering student data

---
+++ src/features/table/tableApi.ts
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { TableItem } from "./tableSlice";
+import { FilterState } from "@features/filter/filterSlice";
+
+const endpoints = [
+  "viewAllData",
+  "filterData",
+]
+
+export const tableApi = createApi({
+  reducerPath: "tableApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://test.omniswift.com.ng/api/" }),
+  endpoints: (builder) => ({
+    getStudentData: builder.query<TableItem[], void>({
+      query: () => endpoints[0],
+      transformResponse: (response: unknown & {data: {students: TableItem[]}}) => response.data.students,
+    }),
+    getFilteredData: builder.mutation<TableItem[], Partial<FilterState>>({
+      query: (filter: FilterState) => ({
+        url: endpoints[1],
+        method: "POST",
+        body: filter,
+      }),
+      transformResponse: (response: unknown & {data: {students: TableItem[]}}) => response.data.students,
+    }),
+  }),
+});
+
+export const { useGetStudentDataQuery, useGetFilteredDataMutation } = tableApi;
